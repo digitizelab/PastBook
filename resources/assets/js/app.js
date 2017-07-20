21d86946fc941f89a6f2c80381141c5de5d6fa1c
Nishan K <nishan@digitizelab.com>
2017-07-20 22:21:16 +0530

    Configuring the application to use React/Redux for the frontend

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import App from './components/app';
+import reducers from './reducers';
 
-require('./bootstrap');
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App/>
+    </Provider>
+    , document.querySelector('.container'));
